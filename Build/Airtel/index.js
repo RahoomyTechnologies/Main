23c5f409eb906a2e5783dbb5b99b988730dd769d
RahoomyTechnologies <rahoomyawadh@gmail.com>
2025-11-11 19:26:35 +0300

    ZARINE

--- Build/Airtel/index.js
+++ Build/Airtel/index.js
@@ -2,8 +2,62 @@ DISPLAY("",`
 
     <header>
 
-        <p class="AirtelName">Airtel</p>
     
+        <p class="mainholder" >Airtel Uganda</p>
+    
+         <img class="images" src="airtel.png">
+
+        <h1 class="discription">About us!</h1>
+
+        <h2 class="discription"> contact us.</h2>
+
+        <h1 class="discription">Our services</h1>
+
     </header>
+
+    <div class=" mainholdersdiv">
+        
+        <div  class="holders"></div>
+
+        <div class="holders"></div>
+
+    </div>
+
+      <form  >
+    <div class=" main">
+
+        <h1> See what RAHOOMY sent</h1>
+
+         <div  class="holder">
+
+        <input class="box" type="text" placeholder="Email">
+
+        </div>
+
+        <br>
+          <div  class="holder">
+
+         <input class="box" type="password" placeholder="password">
+
+        </div>
+
+         <br>
+
+          <div  class="holder">
+
+        <button class="box">log in</button>
+
+        </div>
+
+        <br>
+
+         <div  class="holder">
+
+        <button class="box">create account</button>
+
+        </div>
+
+        </form>
+     </div>
     
 `);
