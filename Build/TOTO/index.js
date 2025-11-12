be595fa1b517725991116fa16084fdfafdf19ec8
RahoomyTechnologies <rahoomyawadh@gmail.com>
2025-11-12 18:01:54 +0300

    ZARINE

---
+++ Build/TOTO/index.js
@@ -0,0 +1,81 @@
+let BODY= document.querySelector("body");
+
+BODY.innerHTML=`
+
+      <header>
+
+        <div class="RightHolder">
+
+            <h3 class="RightText">LEARNING WITH FUN </h3>
+
+            <h2  id="Title" class="RightText" >toto</h2>
+
+        </div>
+
+    </header>
+
+     <div class="Main">
+
+        <div class="WordsHolder" >
+
+                <div class="Ahmed" id="ali">
+
+                      <p class="holder">MAKE A MAGNET</p>
+
+                </div>
+
+                <div class="Maryam" id="">
+                    <h1>Did you know with the help of a trusted adult,
+                        you could make a magnet using electricity? Atrusted
+                        adult should help you at all stage of the experiment.</h1>
+                   
+                </div>
+            <div>
+            
+                <h5 class="zarish"> WHAT YOU WILL NEED</h5>
+            
+            </div>
+            
+            <div class="wordsbox">
+        
+                <h6 class="hanani">1 A large iron nail(about 3 inches)</h6>
+
+            </div>
+
+            <div class="wordsbox">
+
+                <h6 class="saleh">2 about 3 feet of thin coated copper wire</h6>
+
+            </div>
+
+            <div class="wordsbox">
+
+                <h6 class="momy">3 A fresh D size battery</h6>
+
+            </div>
+
+            <div class="wordsbox">
+
+                <h6 class="dady">4 Some paper clips or other small magnetic objects</h6>
+
+            </div>
+
+            <div class="wordsbox">
+
+                <h5 class="be"> INSTRUCTIONS </h5>
+
+            </div>
+
+        </div>
+
+        <div>
+
+            <img src="./Build/TOTO/battery.jpg" class ="images" id="box">
+
+        </div>
+        
+    </div>
+    
+       
+
+`;
